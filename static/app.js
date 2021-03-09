bf3c7f766c4eaa546a2dae9884eebd0456333c44
CReilly0 <cormacreilly0@gmail.com>
2021-03-09 13:12:26 +0000

    Initial draft - test script

---
+++ static/app.js
@@ -0,0 +1,37 @@
+<script>
+    let map;
+
+    function initMap() {
+        fetch("/stations").then(response => {
+            return = response.json();
+        }).then(data => {
+            console.log("data: ", data);
+
+            map = new google.maps.Map(document.getElementById("map"), {
+                center: { lat: 53.349804, lng: -6.260310 },
+                zoom: 13;
+            });
+
+
+        data.forEach(station => {
+            const marker = new google.maps.Marker({
+                position: {lat: station.pos_lat, lng: station.pos_lng},
+                //label: station.name,
+                map: map,
+            });
+
+            marker.addListener("click", () => {
+                const infowindow = new google.maps.InfoWindow({
+                    content: '<h1> ' + station.name + '</h1><b>' + station.bike_stands + '</b>'
+
+                });
+                infowindow.open(map, marker);
+            });
+        });
+
+        }).catch(err => {
+            console.log("Oops!", err);
+        })
+
+    }
+</script>
