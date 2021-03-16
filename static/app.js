1940cd156462de0141a2257f85833601b48fb2c2
Conor Kiy <conorkiy@gmail.com>
2021-03-16 13:05:23 +0000

    FIX: Remove script tags

--- static/app.js
+++ static/app.js
@@ -1,37 +1,35 @@
-<script>
-    let map;
+let map;
 
-    function initMap() {
-        fetch("/stations").then(response => {
-            return = response.json();
-        }).then(data => {
-            console.log("data: ", data);
+function initMap() {
+    fetch("/stations").then(response => {
+        return = response.json();
+    }).then(data => {
+        console.log("data: ", data);
 
-            map = new google.maps.Map(document.getElementById("map"), {
-                center: { lat: 53.349804, lng: -6.260310 },
-                zoom: 13;
-            });
-
-
-        data.forEach(station => {
-            const marker = new google.maps.Marker({
-                position: {lat: station.pos_lat, lng: station.pos_lng},
-                //label: station.name,
-                map: map,
-            });
-
-            marker.addListener("click", () => {
-                const infowindow = new google.maps.InfoWindow({
-                    content: '<h1> ' + station.name + '</h1><b>' + station.bike_stands + '</b>'
-
-                });
-                infowindow.open(map, marker);
-            });
+        map = new google.maps.Map(document.getElementById("map"), {
+            center: { lat: 53.349804, lng: -6.260310 },
+            zoom: 13;
         });
 
-        }).catch(err => {
-            console.log("Oops!", err);
-        })
 
-    }
-</script>
+    data.forEach(station => {
+        const marker = new google.maps.Marker({
+            position: {lat: station.pos_lat, lng: station.pos_lng},
+            //label: station.name,
+            map: map,
+        });
+
+        marker.addListener("click", () => {
+            const infowindow = new google.maps.InfoWindow({
+                content: '<h1> ' + station.name + '</h1><b>' + station.bike_stands + '</b>'
+
+            });
+            infowindow.open(map, marker);
+        });
+    });
+
+    }).catch(err => {
+        console.log("Oops!", err);
+    })
+
+}
