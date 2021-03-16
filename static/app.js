9ef09268f3d0060eeb425d3079be805d3a00c36b
Conor Kiy <conorkiy@gmail.com>
2021-03-16 13:27:29 +0000

    FEAT: Implement infoWindow function

--- static/app.js
+++ static/app.js
@@ -1,31 +1,57 @@
 let map;
 
 function initMap() {
+
+    //Set currentInfoWindow to null
+    var currentInfoWindow = null;
+
     fetch("/stations").then(response => {
-        return = response.json();
+        return response.json();
     }).then(data => {
         console.log("data: ", data);
 
         map = new google.maps.Map(document.getElementById("map"), {
             center: { lat: 53.349804, lng: -6.260310 },
-            zoom: 13;
+            zoom: 14,
         });
 
 
     data.forEach(station => {
         const marker = new google.maps.Marker({
-            position: {lat: station.pos_lat, lng: station.pos_lng},
+            position: {lat: station.position_lat, lng: station.position_long},
             //label: station.name,
             map: map,
         });
 
-        marker.addListener("click", () => {
-            const infowindow = new google.maps.InfoWindow({
-                content: '<h1> ' + station.name + '</h1><b>' + station.bike_stands + '</b>'
-
-            });
-            infowindow.open(map, marker);
+        // Close currentInfoWindow on map click
+        map.addListener("click", () => {
+            if(currentInfoWindow !== null){
+                currentInfoWindow.close();
+            }
         });
+
+        // Add onClick() function to station marker
+        marker.addListener("click", () => {
+
+            // If open close currentInfoWindow
+            if(currentInfoWindow !== null){
+                currentInfoWindow.close();
+            }
+            
+            var banking = "Unavailable";
+            if(station.banking == 1){banking = "Available"};
+
+            // Create infoWindow for station marker
+            var infoWindow = new google.maps.InfoWindow({
+                content:'<h3> ' + station.name + '</h3><b>Stands: </b>' + station.stands + '<br><b>Banking: </b>' + banking
+            });
+
+            // Open infoWindow and assign to currentInfoWindow
+            infowindow.open(map, marker);
+            currentInfoWindow = infoWindow;
+
+        });
+        
     });
 
     }).catch(err => {
@@ -33,3 +59,6 @@ function initMap() {
     })
 
 }
+
+// Call map function
+initMap();
