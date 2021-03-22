84230834fba5bd3d8b93572aeb0a8fc8778e244e
Faye Arejola <faye.arejola98@gmail.com>
2021-03-22 19:15:17 +0000

    Debugged a few lines and added a new query
    new query filters through to show just one of each most recent updated stations

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def location():
 @app.route("/stands")
 def availability():
     engine2 = create_engine(f"mysql+mysqlconnector://{user}:{password}@{uri}:{port}/{db}", echo=True)
-    query2 = "select status, last_update, station_num, avail_stands, avail_bikes from availability;"
+    query2 = "select status, last_update, number, avail_stands, avail_bikes from availability;"
     df2 = pd.read_sql_query(query2, engine2)
     # print(df2)
     return df2.to_json(orient='records')
@@ -49,6 +49,16 @@ def weather():
     df3 = pd.read_sql_query(query3, engine3)
     # print(df3)
     return df3.to_json(orient='records')
+
+
+#parsing from availability table that shows the most recent updated rows from each number
+@app.route("/chosen_station")
+def station():
+    engine4 = create_engine(f"mysql+mysqlconnector://{user}:{password}@{uri}:{port}/{db}", echo=True)
+    query4 = "SELECT number, status, avail_bikes, avail_stands, MAX(last_update) as lastUpdate FROM availability GROUP BY number;"
+    df4 = pd.read_sql_query(query4, engine4)
+    #print(df4)
+    return df4.to_json(orient='records')
 
 
 if __name__ == "__main__":
