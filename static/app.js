072c4f129cfdde675db2bace06d4a89dca940210
Faye Arejola <faye.arejola98@gmail.com>
2021-03-24 11:51:13 +0000

    Changed element heading for address of stations

--- static/app.js
+++ static/app.js
@@ -1,6 +1,7 @@
 let map;
 
 function initMap() {
+    //attempting to
 
     //Set currentInfoWindow to null
     var currentInfoWindow = null;
@@ -76,7 +77,9 @@ function dropDownStations() {
     }).then(stationData => {
 
         console.log(stationData);
-        let eachStation = "<select name='station' id='selection' onchange='showStation()'>" + "<option>Select a Station</option>";
+        let eachStation = "<label for='station'>Stations</label>" +
+                          "<select name='station' id='selection' onchange='showStation()'>" +
+                          "<option>Select a Station</option>";
         //for loop to access stations json
         stationData.forEach(station => {
 
@@ -119,7 +122,7 @@ function showStation() {
         let address = id.options[id.selectedIndex].text;
         let update = new Date(stationInfo.lastUpdate * 1000);
         let stationTable =
-            "<h1>" + address + "</h1>" +
+            "<h2>" + address + "</h2>" +
             "<h3>Status</h3>" +
             "<p>" + stationInfo.status + "</p>" +
             "<h3>Available Bikes</h3>" +
