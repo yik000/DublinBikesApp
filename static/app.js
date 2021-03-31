10a5817ed0049453fff9b4d769416c3401aab373
CReilly0 <cormacreilly0@gmail.com>
2021-03-31 21:11:46 +0100

    FEAT: Added Geolocation javascript button

--- static/app.js
+++ static/app.js
@@ -1,4 +1,4 @@
-let map;
+let map, infoWindow;
 
 function initMap() {
     //attempting to
@@ -65,7 +65,37 @@ function initMap() {
             });
     
         });
-    
+
+        // Add Geolocation services
+        infoWindow = new google.maps.InfoWindow();
+        const locationButton = document.createElement("button");
+        locationButton.textContent = "Pan to Current Location";
+        locationButton.classList.add("custom-map-control-button");
+        map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+        locationButton.addEventListener("click", () => {
+          // Try HTML5 geolocation
+          if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+              (position) => {
+                const pos = {
+                  lat: position.coords.latitude,
+                  lng: position.coords.longitude,
+                };
+                infoWindow.setPosition(pos);
+                infoWindow.setContent("Location found.");
+                infoWindow.open(map);
+                map.setCenter(pos);
+              },
+              () => {
+                handleLocationError(true, infoWindow, map.getCenter());
+              }
+            );
+          } else {
+            // Browser doesn't support Geolocation
+            handleLocationError(false, infoWindow, map.getCenter());
+          }
+        });
+
     }).catch(err => {
         console.log("Oops!", err);
     })
