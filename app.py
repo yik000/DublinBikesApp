f553c5e1b66a649314a06b4d03359fcef519c82d
Faye Arejola <faye.arejola98@gmail.com>
2021-04-09 19:52:10 +0100

    added the forecastInfo query
    - deleted traceback module

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ from flask import Flask, render_template
 from sqlalchemy import create_engine
 import pandas as pd
 import dbinfo
-import traceback
 import datetime
+import requests
+import forecastInfo
 
 # dbinfo
 user = dbinfo.USER
@@ -104,6 +105,18 @@ def daily_availability(stationNum):
     """
     df5 = pd.read_sql_query(query5, engine5)
     return df5.to_json(orient='records')
+
+
+# Get prediction input from user
+@app.route("/predictionInput/<int:station_num>", methods=['POST'])
+def data(station_num):
+    user_input = request.form.to_dict()
+    user_input['station'] = station_num
+    # E.g. -> user_input = { predict_dt : "2021-04-22T19:34", station: 107 }
+    # Query OWM for forecasted weather in Dublin at this time and date
+    forecastInfo.main_weather(user_input)
+    # pass time, station number, and weather into prediction model
+    #return user_input
 
 
 if __name__ == "__main__":
