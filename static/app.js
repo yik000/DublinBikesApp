5a7d5e706af49cb5b80ac6f82ac3a5ec86966d7b
Conor Kiy <conorkiy@gmail.com>
2021-04-11 22:53:59 +0100

    FEAT: Implement directions calculator function

--- static/app.js
+++ static/app.js
@@ -760,3 +760,26 @@ window.onload = function() {
 
     }
 }
+
+
+// Directions calculator function
+function calculateAndDisplayRoute(directionsService, directionsRenderer) {
+    directionsService.route(
+        {
+            origin: {
+                query: document.getElementById("start").value,
+            },
+            destination: {
+                query: document.getElementById("end").value,
+            },
+            travelMode: google.maps.TravelMode.BICYCLING,
+        },
+        (response, status) => {
+            if(status === "OK") {
+                directionsRenderer.setDirections(response);
+            } else {
+                window.alert("Directions request failed due to " + status);
+            }
+        }
+    );
+};
