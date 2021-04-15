b9397a45941a005fa5cd21969b162a0bb4800894
Conor Kiy <conorkiy@gmail.com>
2021-04-15 15:30:02 +0100

    REFACTOR: Add whitespace and newline at EOF

--- forecastInfo.py
+++ forecastInfo.py
@@ -28,7 +28,7 @@ def main_weather(user_input):
         dictionary_weather = [result['list'][i] for i in range(len(result['list'])) if result['list'][i]['dt'] ==
                               final_date]
         if ['rain'] in dictionary_weather: 
-            weather = [dictionary_weather[0]['speed'],dictionary_weather[0]['rain'],
+            weather = [dictionary_weather[0]['speed'], dictionary_weather[0]['rain'],
                        dictionary_weather[0]['temp']['day']]
         elif ['rain'] not in dictionary_weather:
             weather = [dictionary_weather[0]['speed'], 0, dictionary_weather[0]['temp']['day']]
